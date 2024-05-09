97a9e51b0948a9053df68f1541d75acb528e5fe1
swon-04 <swon8468@gmail.com>
2024-05-09 10:37:15 +0900

    0509

--- 0509/0509.py
+++ 0509/0509.py
@@ -113,3 +113,25 @@ for x in members.values():
 
 print("회원 점수 평균 =", total / len(members))
 '''
+
+'''
+books = {
+    '여행의 이유' : 13500,
+    '소년이로' : 13000,
+    '희랍인 조르바' : 9000,
+    '세 여자' : 14000,
+    '아픔이 길이 되려면' : 10000
+}
+print("검색 가능 도서 :", list(books.keys()))
+print('-'*35)
+
+while True:
+    bookName = input("도서명 입력(검색 종료는 0) : ")
+    if bookName in books:
+        print(bookName, "=", books.get(bookName), "원\n")
+    elif bookName == '0':
+        print("프로그램을 종료합니다.")
+        break
+    else:
+        print("검색 가능한 도서가 아닙니다.\n")
+'''
